267e3801755188fe09ae5f259fb941615bda0925
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-05-20 17:08:26 -0300

    Add gatsby-imag package

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/src/images`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
