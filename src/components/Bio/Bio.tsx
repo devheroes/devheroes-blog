59ae77e54c07329f3719c7963bc3ede21bdab679
Marcelo Dapper <marcelo.dapper@gmail.com>
2020-05-20 17:12:02 -0300

    Fix Bio to user gatsby-image

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import Img from 'gatsby-image';
 
 import { rhythm } from '../../utils/typography';
 import { authors } from '../../utils/authors';
 
 type Props = {
   author: string;
+  avatars: {
+    edges: Array<{
+      node: {
+        childImageSharp: {
+          fixed: GatsbyImageSharpFixed;
+        };
+      };
+    }>;
+  };
 };
 
-export default function Bio({ author }: Props) {
+export default function Bio({ author, avatars }: Props) {
+  const avatar = avatars.edges.find(avatar =>
+    avatar.node.childImageSharp.fixed.src.includes(author)
+  );
+
   return (
     <Container>
-      <Image src={authors[author].profilePic} alt={authors[author].name} />
+      {avatar && <Img fixed={avatar.node.childImageSharp.fixed} />}
       <P>
         <a href={authors[author].url}>{authors[author].name}</a> —{' '}
         {authors[author].bio}
@@ -23,16 +37,13 @@ export default function Bio({ author }: Props) {
 const Container = styled.div`
   display: flex;
   margin-bottom: ${rhythm(2)};
-`;
 
-const Image = styled.img`
-  margin-right: ${rhythm(1 / 2)};
-  margin-bottom: 0;
-  width: ${rhythm(2)};
-  height: ${rhythm(2)};
-  border-radius: 50%;
+  img {
+    border-radius: 50%;
+  }
 `;
 
 const P = styled.p`
   max-width: 310px;
+  margin-left: 16px;
 `;
