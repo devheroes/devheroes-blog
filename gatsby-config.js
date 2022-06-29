b467d136564c58bd00cd3440b0d14b66e300590e
Marcelo Dapper <marcelo.dapper@gmail.com>
2022-06-29 13:15:05 -0400

    Install gatsby-remark-prismjs-copy-button

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
             },
           },
           'gatsby-remark-autolink-headers',
+          `gatsby-remark-prismjs-copy-button`,
           {
             resolve: 'gatsby-remark-prismjs',
             options: {
